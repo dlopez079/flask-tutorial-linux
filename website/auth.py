e1cb6b5af2a3e194c02d223922992fc2379928c2
David Lopez <dlopez@mattmen.com>
2022-01-26 14:29:55 -0500

    Added Authentication Routes. I added three authentication routes > Login, Logout, and Sign up.

--- website/auth.py
+++ website/auth.py
@@ -2,3 +2,21 @@ from flask import Blueprint
 
 # We are going to define that this is a blueprint
 auth = Blueprint('auth', __name__) 
+
+# Define the login route
+# Decorator along with address then you define the function.
+@auth.route('/login')
+def login():
+    return "<p>Login</p>"
+
+# Define the login route
+# Decorator along with address then you define the function.
+@auth.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+# Define the login route
+# Decorator along with address then you define the function.
+@auth.route('/sign-up')
+def signup():
+    return "<p>Sign Up!</p>"

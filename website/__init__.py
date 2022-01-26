83104220da2633c984a046070d3825198608e11c
David Lopez <dlopez@mattmen.com>
2022-01-26 14:09:13 -0500

    Refactored code.

--- website/__init__.py
+++ website/__init__.py
@@ -4,12 +4,12 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'al;dkfhjka;dfja;dlfkja;dfj'
 
-    # Let's tell the app that I inserted a blueprint for views.
+    # Intruct the app that I inserted a blueprint for both views and auth
     from .views import views
-    app.register_blueprint(views, url_prefix='/')
-
-    # Let's tell the app that I insereted a blueprint for authentication.
     from .auth import auth
+
+    
+    app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
     return app

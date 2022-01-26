357318b6511e928f4af4a9562de3524185601913
David Lopez <dlopez@mattmen.com>
2022-01-26 17:09:04 -0500

    Main Modification. I removed the sqlalchemy import and capitalized the T in True which was giving me errors.

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from sqlalchemy import true
-#import website initialization
 from website import create_app
 
 app = create_app()
@@ -7,4 +5,4 @@ app = create_app()
 # only if we run this file, run the application in debug mode.  Anytime that we change the code, it will rerun the website server. 
 # We will only enable the debug feature when in development.  We will not have it in production. 
 if __name__ == '__main__':
-    app.run(debug=true)
+    app.run(debug=True)
